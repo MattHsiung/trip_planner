58ee88da74970fa443e614bf93ea7dcba8435d7d
Matt Hsiung <xung@musician.com>
2016-02-02 13:55:04 -0500

    progress

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var app= express();
 var port = 3000;
+var path = require('path')
 //----------------
-app.use(express.static(__dirname+ '/public'));
+// app.use(express.static(__dirname+ '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
@@ -17,6 +18,13 @@ swig.setDefaults({cache: false});
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
+
+// app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+// app.use(express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap',express.static(path.join(__dirname, "./node_modules/bootstrap/dist")));
+app.use('/jquery', express.static(path.join(__dirname, "./node_modules/jquery/dist")));
+app.use('/public', express.static(path.join(__dirname, "./public")));
+
 app.use('/', router)
 
 
@@ -24,19 +32,17 @@ app.use('/', router)
 //routing static files
 
 //404 error
-// app.use()
+app.use(function(req,res,next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+})
 //error handling
 app.use(function(err, req, res, next){
 	res.status(err.status||500);
 	//render error
 
 })
-
-
-
-
-
-
 
 app.listen(port, function(){
 	console.log('listening on port '+port+'...')
--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,38 @@ var router = express.Router();
 var Bluebird = require('bluebird')
 
 var models = require('../models');
-var Hotel = models.Hotel;
 var Restaurant = models.Restaurant;
 var Place = models.Place;
+var Hotel = models.Hotel;
 var Activity = models.Activity;
 
 router.get('/', function (req, res, next){
-	Bluebird.all([
-		Hotel.find({}),
-		Restuarant.find({}),
-		Activity.find({}),
-		Place.find({}),
 
-	])
-	.spread(function(hotels, restaurants, activities){
+	 Bluebird.all([
+	   Hotel.find({}),
+	   Restaurant.find({}), 
+	   Activity.find({})
+	 ])
+	 .spread(function(hotels, restaurants, activities){
+	       res.render('index',{
+	           hotels: hotels,
+	           activities: activities,
+	           restaurants:restaurants
+	       })
+	 	
+	 })		
+})
 
-	})
-	res.send('hi')
-
+router.get('/map', function(req,res,next){
+	res.render('map')
 
 })
+
+// router.use(function(req,res,next){
+// 	var err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// })
 
 router.get('/trip', function (req, res, next){
 	res.send({message:'hi'})
