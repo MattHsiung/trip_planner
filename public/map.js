7b4c1ab95f7480aacd028938770c114b9c03a67c
sp6pe <sp6pe@virginia.edu>
2016-02-02 15:57:30 -0500

    status

--- public/map.js
+++ public/map.js
@@ -1,25 +1,70 @@
-function initialize_gmaps() {
-    // initialize new google maps LatLng object
-    var myLatlng = new google.maps.LatLng(40.705189,-74.009209);
-    // set the map options hash
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    // get the maps div's HTML obj
-    var map_canvas_obj = document.getElementById("map-canvas");
-    // initialize a new Google Map with the options
-    var map = new google.maps.Map(map_canvas_obj, mapOptions);
-    // Add the marker to the map
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title:"Hello World!"
-    });
-    // Add the marker to the map by calling setMap()
-    marker.setMap(map);
+var models = require('../models');
+var Restaurant = models.Restaurant;
+var Place = models.Place;
+var Hotel = models.Hotel;
+var Activity = models.Activity;
+
+
+
+
+// function findLocation(name){
+//     Hotel.findOne({'name':name}).exec().then(function(hotelInfo){
+//         console.log (hotelInfo.place[0].location);
+//     })
+// }
+// findLocation('Hotel 91');
+
+
+
+
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    scrollwheel: false,
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
 }
 
-$(document).ready(function() {
-    initialize_gmaps();
-});
+
+map.set('styles', [
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      { color: '#000000' },
+      { weight: 1.6 }
+    ]
+  }, {
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      { saturation: -100 },
+      { invert_lightness: true }
+    ]
+  }, {
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      { hue: '#ffff00' },
+      { gamma: 1.4 },
+      { saturation: 82 },
+      { lightness: 96 }
+    ]
+  }, {
+    featureType: 'poi.school',
+    elementType: 'geometry',
+    stylers: [
+      { hue: '#fff700' },
+      { lightness: -15 },
+      { saturation: 99 }
+    ]
+  }
+]);
